ac14a2b31ce428ca36c0237453588ce4f86e185d
Wendy Van Craen <wendy.vancraen@gmail.com>
2020-02-05 16:33:13 +0100

    part 1 of challenge done

--- other-courses/Udemy_CompleteJS/04-OOP/challenge.js
+++ other-courses/Udemy_CompleteJS/04-OOP/challenge.js
@@ -10,45 +10,58 @@ b) the answers from which the player can choose the correct one (choose an adequ
 c) correct answer (I would use a number for this)
 */
 
-var questions = [];
+(function () {
 
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
-
-// 2. Create a couple of questions using the constructor
-var q1 = new Question("What species is Jabba from Star Wars?",["Ithorian", "Jenet","Jawa", "Hutt"],3);
-var q2 = new Question("What\'s the name of the spaceship that can destroy a whole planet?", ["Planet destroyer", "Death Star", "Stargun"], 1);
-var q3 = new Question("What\'s the name of the big villain in Star Wars?",["Sint", "Vader", "Sith Lord", "Darth Lord"], 2);
-var q4 = new Question("Which robot is not a character in Star Wars?",["Wal-E","C-3PO", "R2-D2", "BB-8"],0);
-var q5 = new Question("What\'s the name of Han Solo\'s ship?",["Tardis","Millenium Falcon", "Serenity", "Rocinante"], 1);
-
-// 3. Store them all inside an array
-questions.push(q1, q2, q3, q4, q5);
-
-// 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
-function randomQuestion () {
-    var randomQ = Math.floor(Math.random() * 5);
-
-    console.log(questions[randomQ].question);
-
-    for (var i = 0; i < questions[randomQ].answers.length; i++) {
-        console.log(i + ": " + questions[randomQ].answers[i]);   
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
     }
-}
 
-randomQuestion();
+    Question.prototype.display = function () {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ": " + this.answers[i]);
+        }
+    };
+
+    Question.prototype.checkAnswer = function (answer) {
+        if (answer === this.correct) {
+            console.log("Correct answer!");
+        } else {
+            console.log("That is not the right answer.");
+        };
+    }
+
+    // 2. Create a couple of questions using the constructor
+    var q1 = new Question("What species is Jabba from Star Wars?", ["Ithorian", "Jenet", "Jawa", "Hutt"], 3);
+    var q2 = new Question("What\'s the name of the spaceship that can destroy a whole planet?", ["Planet destroyer", "Death Star", "Stargun"], 1);
+    var q3 = new Question("What\'s the name of the big villain in Star Wars?", ["Sint", "Vader", "Sith Lord", "Darth Lord"], 2);
+    var q4 = new Question("Which robot is not a character in Star Wars?", ["Wal-E", "C-3PO", "R2-D2", "BB-8"], 0);
+    var q5 = new Question("What\'s the name of Han Solo\'s ship?", ["Tardis", "Millenium Falcon", "Serenity", "Rocinante"], 1);
+
+    // 3. Store them all inside an array
+    var questions = [q1, q2, q3, q4, q5];
+
+    // 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+    var randomQ = Math.floor(Math.random() * questions.length);
+
+    questions[randomQ].display();
+
+    //5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+    var answer = parseInt(prompt("Please enter the number of the correct answer."));
+
+    // 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+    questions[randomQ].checkAnswer(answer);
+
+})();
+
+// 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+
 
 
 /*
-5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
-
-6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
-
-7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
-
 --- Expert level ---
 
 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
