7de3b89f2ad7782ca66b47405ca1ea789384d141
Wendy Van Craen <wendy@appsaloon.be>
2020-10-04 11:12:27 +0200

    setup mvc budgetapp

--- other-courses/Udemy_CompleteJS/05-BudetApp/app.js
+++ other-courses/Udemy_CompleteJS/05-BudetApp/app.js
@@ -1,212 +1,42 @@
-// BUDGET CONTROLLER
-var budgetController = (function () {
+//Model
+var budgetController = (function() {
 
-    var Expense = function (id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
+    var x= 23;
 
-    var Income = function (id, description, value) {
-        this.id = id;
-        this.description = description;
-        this.value = value;
-    };
-
-    var data = {
-        allItems: {
-            exp: [],
-            inc: []
-        },
-        totals: {
-            exp: 0,
-            inc: 0
-        }
-    };
-
-    return {
-        addItem: function (type, des, val) {
-
-            var newItem, ID;
-
-            // Create new ID
-            if (data.allItems[type].length > 0) {
-                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-            } else {
-                ID = 0;
-            }
-
-
-            // Create new item based on 'inc' or 'exp' type
-            if (type === 'exp') {
-                newItem = new Expense(ID, des, val);
-            } else if (type === 'inc') {
-                newItem = new Income(ID, des, val);
-            }
-
-            // Push it in our data structure
-            data.allItems[type].push(newItem);
-
-            // Return the new element
-            return newItem;
-        },
-
-        calculateBudget: function() {
-
-            // calculate total income and expenses
-
-
-            // calculate the budget: income - expenses
-
-
-            // calculate the percentage of income that we spent
-
-        },
-
-        // Use testing methods for development to expose internal data
-        testing: function () {
-            console.log(data);
-        }
-
-    };
-
-})();
-
-
-
-
-// UI CONTROLLER
-var UIController = (function () {
-
-    var DOMstrings = {
-        inputType: '.add__type',
-        inputDescription: '.add__description',
-        inputValue: '.add__value',
-        inputBtn: '.add__btn',
-        incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+    var add = function(a) {
+        return x + a;
     }
 
     return {
 
-        getInput: function () {
-            return {
-                type: document.querySelector(DOMstrings.inputType).value, // "inc" or "exp"
-                description: document.querySelector(DOMstrings.inputDescription).value,
-                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
-            };
-        },
-
-        addListItem: function (obj, type) {
-
-            var html, newHtml, element;
-
-            // Create HTML string with placeholder text
-            if (type === 'inc') {
-                element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-            } else if (type === 'exp') {
-                element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-            }
-
-            // Replace the placeholder text with some actual data
-            newHtml = html.replace('%id%', obj.id);
-            newHtml = newHtml.replace('%description%', obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
-
-            // Insert HTML into the DOM
-            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
-
-        },
-
-        clearFields: function () {
-
-            var fields, fieldsArr;
-
-            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
-
-            fieldsArr = Array.prototype.slice.call(fields);
-
-            fieldsArr.forEach(function (current, index, array) {
-                current.value = "";
-            });
-
-            fieldsArr[0].focus();
-        },
-
-        getDOMstrings: function () {
-            return DOMstrings;
+        publicTest: function(b) {
+            return add(b);
         }
-    };
+
+    }
 
 })();
 
 
+//Viewer
+var UIController = (function() {
+
+    // Some Code
+
+})();
 
 
-// GLOBAL APP CONTROLLER
-var controller = (function (budgetCtrl, UICtrl) {
+//Controller
+var controller = (function(budgetCtrl, UICtrl) {
 
-    var setupEventListeners = function () {
-
-        var DOM = UICtrl.getDOMstrings();
-
-        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-        document.addEventListener('keypress', function (event) {
-            if (event.keycode === 13 || event.which === 13) {
-                ctrlAddItem();
-            }
-        });
-    };
-
-    var updateBudget = function () {
-
-        // Calculate budget
-
-        // Return the budget
-
-        // Display budget in the UI
-
-    };
-
-
-
-    var ctrlAddItem = function () {
-
-        var input, newItem;
-
-        // Get the filed input data
-        input = UICtrl.getInput();
-
-
-        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
-
-            // Add the item to the budget controller
-            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-
-            // Add the new item to the UI
-            UICtrl.addListItem(newItem, input.type);
-
-            // Clear the fields
-            UICtrl.clearFields();
-
-            // Calculate  and update budget
-            updateBudget();
-
-        };
-
-
-    };
+    var z = budgetCtrl.publicTest(5);
 
     return {
-        init: function () {
-            console.log('Application has started.');
-            setupEventListeners();
+
+        anotherPublic: function() {
+            console.log(z);
         }
-    };
+
+    }
 
 })(budgetController, UIController);
-
-controller.init();
