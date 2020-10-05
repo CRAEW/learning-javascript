e379b559a3f8e0522d153b754e65613e317b33b3
Wendy Van Craen <wendy@appsaloon.be>
2020-10-05 08:59:55 +0200

    added newItem to budgetController

--- other-courses/Udemy_CompleteJS/05-BudetApp/app.js
+++ other-courses/Udemy_CompleteJS/05-BudetApp/app.js
@@ -1,7 +1,61 @@
 // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    // Some Code
+    var Expense = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
+
+    var Income = function(id, description, value) {
+        this.id = id,
+        this.description = description,
+        this.value = value
+    };
+
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length -1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            // PUsh into data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new element
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+
+    };
 
 })();
 
@@ -59,10 +113,13 @@ var controller = (function(budgetCtrl,UICtrl) {
 
     // Adds new item
     var ctrlAddItem = function() {
+        var input, newItem;
+
         // Get the input data
-        var input = UICtrl.getinput();
+        input = UICtrl.getinput();
 
         // Add the item to budget controller
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // Add item to UI
 
