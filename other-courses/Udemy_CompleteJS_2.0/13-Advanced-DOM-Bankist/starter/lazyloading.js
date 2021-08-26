008a9ec623fbb0a3f0db8b2343d8e5ad06cd3617
Wendy <wendy@appsaloon.be>
2021-08-26 18:17:14 +0200

    Added lazy load images

--- other-courses/Udemy_CompleteJS_2.0/13-Advanced-DOM-Bankist/starter/lazyloading.js
+++ other-courses/Udemy_CompleteJS_2.0/13-Advanced-DOM-Bankist/starter/lazyloading.js
@@ -5,7 +5,7 @@ const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
+  //   console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -22,3 +22,30 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+// lazy load images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, oberser) {
+  const [entry] = entries;
+  //   console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  oberser.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
