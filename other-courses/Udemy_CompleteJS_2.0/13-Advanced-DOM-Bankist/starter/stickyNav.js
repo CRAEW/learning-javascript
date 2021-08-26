f875e1716a75abe4c40206e5349a6a7052c40925
Wendy <wendy@appsaloon.be>
2021-08-26 17:32:58 +0200

    IntersectionObserver - reveal elements on scroll

--- other-courses/Udemy_CompleteJS_2.0/13-Advanced-DOM-Bankist/starter/stickyNav.js
+++ other-courses/Udemy_CompleteJS_2.0/13-Advanced-DOM-Bankist/starter/stickyNav.js
@@ -29,11 +29,11 @@
 // Sticky header version 2
 const headerNav = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
+// console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
