e980c1f3f473975397a717835f434040b4d26818
Wendy <wendy@appsaloon.be>
2021-11-29 17:02:12 +0100

    ES6 Classes: Chaining methods

--- other-courses/Udemy_CompleteJS_2.0/14-OOP/starter/Account.js
+++ other-courses/Udemy_CompleteJS_2.0/14-OOP/starter/Account.js
@@ -1,30 +1,41 @@
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+// (there is also the static version)
+
 class Account {
+  // Public Fields (instances)
+  locale = navigator.language;
+
+  // Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     // Protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
-  //   Public interface
+  // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
+    return this;
   }
 
   withddraw(val) {
     this.deposit(-val);
-  }
-
-  _approveLoan(val) {
-    return true;
+    return this;
   }
 
   requestLoan(val) {
@@ -32,12 +43,34 @@ class Account {
       this.deposit(val);
       console.log('Loan approved');
     }
+    return this;
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // Private methods
+  _approveLoan(val) {
+    return true;
   }
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withddraw(140);
 acc1.requestLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
+console.log(acc1._pin);
+// console.log(acc1.#movements);
+Account.helper();
+
+// Chaining
+acc1.deposit(300).deposit(500).withddraw(35).requestLoan(25000).withddraw(4000);
+console.log(acc1.getMovements());
